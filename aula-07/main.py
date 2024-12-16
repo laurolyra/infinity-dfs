27695295c6f295d5c3a691f5b45c91ef7de548d5
Lauro Lyra <laurolyra@gmail.com>
2024-12-16 20:26:19 -0300

    enunciado e código esclarecido

--- aula-07/main.py
+++ aula-07/main.py
@@ -2,29 +2,54 @@ opcao = int(input("selecione uma opção: "))
 def menu_principal():
     if opcao == 1:
         print("opcao 1")
+        #chame uma função de chave_pix
     if opcao == 2:
         print("opcao 2")
+        #chame uma função de chave_pix
     if opcao == 3:
         print("opcao 3")
+        #chame uma função de chave_pix
     if opcao == 4:
         print("opcao 4")
+        #chame uma função de chave_pix
     if opcao == 5:
         print("opcao 5")
+        #chame uma função de dados_pessoais
     if opcao == 6:
         print("opcao 6")
+        #chame uma função de dados_pessoais
     if opcao == 7:
         print("opcao 7")
+        #chame uma função de dados_pessoais
     if opcao == 8:
         print("opcao 8")
+        #chame uma função de dados_pessoais
     if opcao == 9:
         print("opcao 9")
+        #chame uma função de endereco
     if opcao == 10:
         print("opcao 10")
+        #chame uma função de endereco
     if opcao == 11:
         print("opcao 11")
+        #chame uma função de endereco
     if opcao == 12:
         print("opcao 12")
+        #chame uma função de endereco
+    if opcao == 13:
+        print("opcao 13")
+        #chame uma função de telefone
+    if opcao == 14:
+        print("opcao 14")
+        #chame uma função de telefone
+    if opcao == 15:
+        print("opcao 15")
+        #chame uma função de telefone
+    if opcao == 16:
+        print("opcao 16")
+        #chame uma função de telefone
     if opcao == 0:
         print("opcao 0")
+        #saia da aplicação
 
 menu_principal()
---
+++ aula-07/telefones.py
@@ -0,0 +1,11 @@
+def cria_telefone():
+	print('cria telefone')
+
+def edita_telefone():
+	print('edita telefone')
+
+def deleta_telefone():
+	print('deleta telefone')
+
+def gera_qr_code_telefone():
+	print('gera qr code do telefone')
