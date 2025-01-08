1dda3bdbe3a24d58961cef30fd45664dd34c9ef1
Lauro Lyra <laurolyra@gmail.com>
2025-01-08 15:31:16 -0300

    revisaço

---
+++ revisaco/listas/exercicios_de_listas.py
@@ -0,0 +1,26 @@
+# 1- complete a função abaixo, fazendo com que ela retorne a lista [0, 1, 2, 3, 4, 5]
+def retorna_0_a_5():
+  print('retorna_0_a_5')
+  #remova o print acima e escreva o código aqui
+# 2- complete a função abaixo, que irá receber uma lista de números e, ao final, deverá imprimir
+# cada um dos seus elementos no terminal
+def imprime_elementos_da_lista(lista_de_numeros):
+  print('imprime_elementos_da_lista(lista_de_numeros)')
+  #remova o print acima e escreva o código aqui
+# 3- complete a função abaixo, que irá receber uma lista
+# e deverá retornar ela mesma, mas com um "galo_doido" no final dela
+# exemplo: insere_galo_doido([10, 11, 12]) deverá retornar [10, 11, 12, "galo_doido"]
+def insere_galo_doido(lista_qualquer):
+  print('insere_galo_doido(lista_qualquer)')
+  #remova o print acima e escreva o código aqui
+# 4- complete a função abaixo, que irá receber uma quantidade indeterminada de argumentos
+# e deverá retornar uma lista com eles
+# exemplo: cria_lista('aula', 'revisão', 'args', 'kwargs') deverá retornar ['aula', 'revisão', 'args', 'kwargs']
+def cria_lista():# você precisará mexer nos argumentos também!
+  print('imprime_elementos_da_lista(lista_de_numeros)')
+  #remova o print acima e escreva o código aqui
+
+
+# boa sorte!
+#dada a lista abaixo, escreva um código que modifique essa lista, trocando os valores None por "galo_doido".
+lista_com_none = [0, 1, 3, None, 5, 8, 9, None, 10, 13, 15, 16, 109, None]
